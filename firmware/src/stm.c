85aff499c32485d6857140a9212f11cbc5cdedfe
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-04-21 11:24:28 +0200

    added statement database

---
+++ firmware/src/stm.c
@@ -0,0 +1,106 @@
+#include <esp_random.h>
+
+#include "stm.h"
+#include "sns.h"
+
+stm_entry_t stm_entries[] = {
+    {
+        .text = "Guten Morgen! Heute wird ein herrlicher Tag!",
+        .exclaim = true,
+        .action = STM_FROM_INTRO,
+    },
+    {
+        .text = "Ich kann hier kaum atmen!",
+        .exclaim = true,
+        .co2_min = 2000,
+    },
+    {
+        .text = "Cool, du hast deine Messung abgeschlossen!",
+        .exclaim = true,
+        .action = STM_COMP_MEASUREMENT,
+    },
+    {
+        .text = "Ahhh... Ich liebe frische Luft!",
+        .exclaim = false,
+        .co2_max = 600,
+    },
+    {
+        .text = "Ich bin grad am rechnen…",
+        .exclaim = false,
+        .action = STM_FROM_MEASUREMENT,
+    },
+};
+
+size_t stm_num = sizeof(stm_entries) / sizeof(stm_entry_t);
+
+stm_entry_t* stm_query(bool exclaim, stm_action_t action) {
+  // get sensor data
+  sns_state_t sensor = sns_get();
+
+  // de/select and count entries
+  int selected = 0;
+  for (size_t i = 0; i < stm_num; i++) {
+    // get entry
+    stm_entry_t* entry = &stm_entries[i];
+
+    // set selection
+    entry->selected = true;
+
+    // check exclaim
+    if (entry->exclaim != exclaim) {
+      entry->selected = false;
+    }
+
+    // check action
+    if (entry->action != 0 && action != entry->action) {
+      entry->selected = false;
+    }
+
+    // check co2
+    if (entry->co2_min != 0 && sensor.co2 < entry->co2_min) {
+      entry->selected = false;
+    } else if (entry->co2_max != 0 && sensor.co2 > entry->co2_max) {
+      entry->selected = false;
+    }
+
+    // check temperature
+    if (entry->tmp_min != 0 && sensor.tmp < entry->tmp_min) {
+      entry->selected = false;
+    } else if (entry->tmp_max != 0 && sensor.tmp > entry->tmp_max) {
+      entry->selected = false;
+    }
+
+    // check humidity
+    if (entry->hum_min != 0 && sensor.hum < entry->hum_min) {
+      entry->selected = false;
+    } else if (entry->hum_max != 0 && sensor.hum > entry->hum_max) {
+      entry->selected = false;
+    }
+
+    // increment if selected
+    if (entry->selected) {
+      selected++;
+    }
+  }
+
+  // check selected
+  if (selected == 0) {
+    return NULL;
+  }
+
+  // choose entry randomly
+  selected = (int)esp_random() % selected;
+
+  // find and return entry
+  for (int i = 0; i < stm_num; i++) {
+    stm_entry_t* entry = &stm_entries[i];
+    if (entry->selected) {
+      selected--;
+      if (selected < 0) {
+        return entry;
+      }
+    }
+  }
+
+  return NULL;
+}
---
+++ firmware/src/stm.h
@@ -0,0 +1,33 @@
+#ifndef STM_H
+#define STM_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef enum {
+  STM_FROM_INTRO = 1,
+  STM_FROM_SETTINGS = 2,
+  STM_FROM_ANALYSIS = 3,
+  STM_FROM_MEASUREMENT = 4,
+  STM_START_MEASUREMENT = 5,
+  STM_COMP_MEASUREMENT = 6,
+} stm_action_t;
+
+typedef struct {
+  const char *text;
+  bool exclaim;
+  stm_action_t action;
+  uint16_t co2_min;
+  uint16_t co2_max;
+  float tmp_min;
+  float tmp_max;
+  float hum_min;
+  float hum_max;
+  // ---
+  bool selected;
+} stm_entry_t;
+
+stm_entry_t *stm_query(bool exclaim, stm_action_t action);
+
+#endif  // STM_H
