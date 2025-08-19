443b34eb37621ef60ba86391a33eb9809b3e6b11
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-08-19 14:00:40 +0200

    firmware: improved buzzer parallelism

--- firmware/lib/al/buzzer.h
+++ firmware/lib/al/buzzer.h
@@ -11,7 +11,8 @@ void al_buzzer_click();
  *
  * @param hz The frequency of the tone to play.
  * @param ms The duration of the tone to play.
+ * @param wait If true, waits for the tone to finish before returning.
  */
-void al_buzzer_beep(int hz, int ms);
+void al_buzzer_beep(int hz, int ms, bool wait);
 
 #endif  // AL_BUZZER_H
--- firmware/lib/buzzer.c
+++ firmware/lib/buzzer.c
@@ -1,11 +1,20 @@
+#include <naos.h>
+#include <naos/sys.h>
 #include <driver/rmt_tx.h>
+
+#define BUZZER_DEBUG false
 
 // Component: PKMCS0909E
 
+static naos_mutex_t al_buzzer_mutex;
+static bool al_buzzer_beeping = false;
 static rmt_channel_handle_t al_buzzer_channel;
 static rmt_encoder_handle_t al_buzzer_encoder;
 
 void al_buzzer_init() {
+  // check state
+  al_buzzer_mutex = naos_mutex();
+
   // setup buzzer
   rmt_tx_channel_config_t rmt_cfg = {
       .gpio_num = GPIO_NUM_5,
@@ -23,6 +32,14 @@ void al_buzzer_init() {
 }
 
 void al_buzzer_click() {
+  // check beeping
+  naos_lock(al_buzzer_mutex);
+  bool beeping = al_buzzer_beeping;
+  naos_unlock(al_buzzer_mutex);
+  if (beeping) {
+    return;
+  }
+
   // prepare buzz
   rmt_symbol_word_t item = {
       .level0 = 1,
@@ -39,15 +56,27 @@ void al_buzzer_click() {
   ESP_ERROR_CHECK(rmt_transmit(al_buzzer_channel, al_buzzer_encoder, &item, sizeof(item), &cfg));
 }
 
-void al_buzzer_beep(int hz, int ms) {
+void al_buzzer_beep(int hz, int ms, bool wait) {
   // check arguments
   if (hz == 0 || ms == 0) {
+    return;
+  }
+
+  // check flag
+  naos_lock(al_buzzer_mutex);
+  bool was_beeping = al_buzzer_beeping;
+  al_buzzer_beeping = true;
+  naos_unlock(al_buzzer_mutex);
+  if (was_beeping) {
     return;
   }
 
   // calculate parameters
   int period_us = 1000000 / hz;
   int cycles = (ms * 1000) / period_us;
+  if (BUZZER_DEBUG) {
+    naos_log("al-bzr: beep %dHz for %dms (%d cycles, period %dus)", hz, ms, cycles, period_us);
+  }
 
   // generate waveform
   rmt_symbol_word_t waveform[cycles];
@@ -63,7 +92,17 @@ void al_buzzer_beep(int hz, int ms) {
   // perform tone
   rmt_transmit_config_t cfg = {
       .flags.eot_level = 0,
-      .flags.queue_nonblocking = 1,
   };
   ESP_ERROR_CHECK(rmt_transmit(al_buzzer_channel, al_buzzer_encoder, waveform, sizeof(waveform), &cfg));
+  if (wait) {
+    ESP_ERROR_CHECK(rmt_tx_wait_all_done(al_buzzer_channel, -1));
+  }
+  if (BUZZER_DEBUG) {
+    naos_log("al-bzr: done");
+  }
+
+  // clear flag
+  naos_lock(al_buzzer_mutex);
+  al_buzzer_beeping = false;
+  naos_unlock(al_buzzer_mutex);
 }
